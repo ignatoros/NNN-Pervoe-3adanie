3a38b203613fbefd580cf4523ddfe4a3e292d6ab
oros@tk34.mskobr.ru <oros@tk34.mskobr.ru>
2022-04-25 11:40:51 +0300

    Добавьте файлы проекта.

---
+++ NNN/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 1000;
+            for (int i = 2; i <= N; i++)
+            {
+                int j;
+                for (j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+
+
+
+                }
+
+                if (j > Math.Sqrt(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            {
+
+            }
+        }
+    }
+}
